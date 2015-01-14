bb064cf41c3180f2183f385a99203de25defec20
Chris Antaki <ChrisAntaki@gmail.com>
2015-01-14 00:14:44 -0800

    Tightening up intro animations across devices

--- js/battle.js
+++ js/battle.js
@@ -302,7 +302,9 @@ module.exports = ImagePreloader;
 var html = '<div class="timer-spinner"><div style="-webkit-transform:rotate(0deg) translate(0,-60px);transform:rotate(0deg) translate(0,-60px);"></div><div style="-webkit-transform:rotate(30deg) translate(0,-60px);transform:rotate(30deg) translate(0,-60px);"></div><div style="-webkit-transform:rotate(60deg) translate(0,-60px);transform:rotate(60deg) translate(0,-60px);"></div><div style="-webkit-transform:rotate(90deg) translate(0,-60px);transform:rotate(90deg) translate(0,-60px);"></div><div style="-webkit-transform:rotate(120deg) translate(0,-60px);transform:rotate(120deg) translate(0,-60px);"></div><div style="-webkit-transform:rotate(150deg) translate(0,-60px);transform:rotate(150deg) translate(0,-60px);"></div><div style="-webkit-transform:rotate(180deg) translate(0,-60px);transform:rotate(180deg) translate(0,-60px);"></div><div style="-webkit-transform:rotate(210deg) translate(0,-60px);transform:rotate(210deg) translate(0,-60px);"></div><div style="-webkit-transform:rotate(240deg) translate(0,-60px);transform:rotate(240deg) translate(0,-60px);"></div><div style="-webkit-transform:rotate(270deg) translate(0,-60px);transform:rotate(270deg) translate(0,-60px);"></div><div style="-webkit-transform:rotate(300deg) translate(0,-60px);transform:rotate(300deg) translate(0,-60px);"></div><div style="-webkit-transform:rotate(330deg) translate(0,-60px);transform:rotate(330deg) translate(0,-60px);"></div></div>';
 
 function LoadingIcon(params) {
-    document.querySelector(params.target).innerHTML = html;
+    var target = document.querySelector(params.target);
+    target.innerHTML = html;
+    target.className += ' fadeIn';
 }
 
 module.exports = LoadingIcon;
